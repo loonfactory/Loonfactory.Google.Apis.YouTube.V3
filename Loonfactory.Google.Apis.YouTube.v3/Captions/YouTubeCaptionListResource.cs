9a6ecb7f995810cd381b9ff4a788f56a56820b24
W.S.Son <thsdnjstjq1@naver.com>
2024-10-12 06:18:07 +0000

    Create YouTubeCaptionListResource.

---
+++ Loonfactory.Google.Apis.YouTube.v3/Captions/YouTubeCaptionListResource.cs
@@ -0,0 +1,28 @@
+// Licensed under the MIT license by loonfactory.
+
+using System.Text.Json.Serialization;
+
+namespace Loonfactory.Google.Apis.YouTube.v3.Captions;
+
+/// <summary>
+/// The YouTubeCaptionList represents a list of YouTube caption resources.
+/// </summary>
+class YouTubeCaptionListResource
+{
+    /// <summary>
+    /// Identifies the API resource's type.
+    /// The value will be <c>"youtube#captionListResponse"</c>.
+    /// </summary>
+    public string? Kind { get; set; }
+
+    /// <summary>
+    /// The Etag of this resource.
+    /// </summary>
+    [JsonPropertyName("etag")]
+    public string? ETag { get; set; }
+
+    /// <summary>
+    /// A list of captions that match the request criteria.
+    /// </summary>
+    public IEnumerable<YouTubeCaptionResource>? Items { get; set; }
+}
