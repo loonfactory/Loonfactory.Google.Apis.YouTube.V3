2feea48ceadeee3ea0ceee6e07c3583a1d6ff7da
W.S.Son <thsdnjstjq1@naver.com>
2024-11-15 02:17:23 +0000

    Created I18nRegions

---
+++ src/I18nRegions/YouTubeI18nRegionListResource.cs
@@ -0,0 +1,7 @@
+// Licensed under the MIT license by loonfactory.
+
+namespace Loonfactory.Google.Apis.YouTube.V3.I18nRegions;
+
+public class YouTubeI18nRegionListResource : YouTubeListResource<YouTubeI18nRegionResource>
+{
+}
