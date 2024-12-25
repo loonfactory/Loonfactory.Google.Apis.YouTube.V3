2ab5b7e0ba7af73928a8a00a3f4fd14a4d7ed550
W.S.Son <thsdnjstjq1@naver.com>
2024-12-25 10:57:24 +0900

    Implement Comment thread feature for YouTube Data Api. (#9)

---
+++ src/CommentThreads/YouTubeCommentThreadReplieResource.cs
@@ -0,0 +1,13 @@
+// Licensed under the MIT license by loonfactory.
+
+using Loonfactory.Google.Apis.YouTube.V3.Comments;
+
+namespace Loonfactory.Google.Apis.YouTube.V3.CommentThreads;
+
+/// <summary>
+/// @TODO
+/// </summary>
+public class YouTubeCommentThreadReplieResource
+{
+    public IEnumerable<YouTubeCommentResource>? Comments { get; set; }
+}
