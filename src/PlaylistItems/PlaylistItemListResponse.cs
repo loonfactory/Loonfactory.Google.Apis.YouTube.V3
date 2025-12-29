64f74c178c3e655fddb54abff63cb49a665a5c8c
W.S.Son <thsdnjstjq1@naver.com>
2025-12-29 19:47:20 +0900

    refactor: remove YouTubeListResponse base class from DTOs (#20)

--- src/PlaylistItems/PlaylistItemListResponse.cs
+++ src/PlaylistItems/PlaylistItemListResponse.cs
@@ -1,7 +1,19 @@
 // Licensed under the MIT license by loonfactory.
 
+using System.Text.Json.Serialization;
+
 namespace Loonfactory.Google.Apis.YouTube.V3.PlaylistItems;
 
-public class PlaylistItemListResponse : YouTubeListResponse<PlaylistItemResource>
+public class PlaylistItemListResponse
 {
+    public string? Kind { get; set; }
+    /// <summary>
+    /// The Etag of this resource.
+    /// </summary>
+    [JsonPropertyName("etag")]
+    public string? ETag { get; set; }
+    public string? NextPageToken { get; set; }
+    public string? PrevPageToken { get; set; }
+    public YouTubePageInfo? PageInfo { get; set; }
+    public IEnumerable<PlaylistItemResource>? Items { get; set; }
 }
